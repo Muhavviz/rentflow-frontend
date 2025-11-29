b13cd80f8a6c358cf94b811423d3120ca77eb4a4
Muhavviz <muhavvizbadriya@gmail.com>
2025-11-30 03:24:33 +0530

    vercel debugging

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5555'
+    baseURL: 'https://rentflow-wrrf.onrender.com'
 })
