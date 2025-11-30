56efdb25f2eb9e8002bfcba6ce0e6925897b1f84
Muhavviz <muhavvizbadriya@gmail.com>
2025-11-30 21:50:28 +0530

    base url debug

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://rentflow-wrrf.onrender.com'
+    baseURL: 'https://rentflow-wrrf.onrender.com' || "http://localhost:5555"
 })
